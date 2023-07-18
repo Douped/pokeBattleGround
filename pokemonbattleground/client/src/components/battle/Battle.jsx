89ce67934e89a19701ea805f3d2ef83ca306ab41
Albikras <124819029+Albikras@users.noreply.github.com>
2023-07-17 21:33:08 -0400

    opponent pokemon added

--- pokemonbattleground/client/src/components/battle/Battle.jsx
+++ pokemonbattleground/client/src/components/battle/Battle.jsx
@@ -5,12 +5,16 @@ import {
   QUERY_GET_USER_DATA,
   QUERY_SINGLE,
   QUERY_GET_POKEMON_MOVE_DATA,
+  QUERY_GET_OPPONENT_MOVES,
 } from "../../utils/queries";
 
+import { RANDOMIZEOPPONENTMOVES } from "../../utils/mutations";
+
 const Battle = () => {
+  const [randomizeOpponentMoves] = useMutation(RANDOMIZEOPPONENTMOVES, {});
   let { loading, data, refetch } = useQuery(QUERY_GET_USER_DATA, {});
   const pokemonList = data?.me || [];
-  console.log(pokemonList);
+  // console.log(pokemonList);
 
   let { loading: singlePokemonLoading, data: firstPokemon } = useQuery(
     QUERY_SINGLE,
@@ -18,23 +22,72 @@ const Battle = () => {
   );
 
   const userPokemon = firstPokemon?.singlePokemon || [];
-  console.log(userPokemon);
+  // console.log(userPokemon);
+
+  //copied stuff
+
+  let { loading: opponentLoading, data: opponentQueryData } = useQuery(
+    QUERY_GET_OPPONENT_MOVES,
+    {}
+  );
+  const opponentData = opponentQueryData?.getOpponentMoves || [];
+  // console.log(opponentData);
+
+  let { loading: userMovesLoading, data: userMovesData } = useQuery(
+    QUERY_GET_POKEMON_MOVE_DATA,
+    { variables: { pokemonID: pokemonList.pokemon } }
+  );
+  const userPokemonMovesList = userMovesData?.getPokemonMoveData || [];
+  // console.log(userPokemonMovesList);
+
+  let { loading: opponentMovesLoading, data: opponentMovesData } = useQuery(
+    QUERY_GET_POKEMON_MOVE_DATA,
+    { variables: { pokemonID: opponentData[0]?.pokemon } }
+  );
+
+  const opponentPokemonMovesList = opponentMovesData?.getPokemonMoveData || [];
+  // console.log(opponentPokemonMovesList);
+
+  let { loading: secondSinglePokemonLoading, data: secondPokemon } = useQuery(
+    QUERY_SINGLE,
+    { variables: { pokemonId: opponentData[0]?.pokemon } }
+  );
+
+  const brocksPokemon = secondPokemon?.singlePokemon || [];
+  console.log(brocksPokemon);
+
+  // const userMoves = userPokemon.moves || [];
+  // const opponentMoves = opponentData[0]?.moves || [];
 
   useEffect(() => {
-    if (!loading && !singlePokemonLoading) {
+    if (
+      !loading &&
+      !singlePokemonLoading &&
+      !opponentLoading &&
+      !userMovesLoading &&
+      !opponentMovesLoading
+    ) {
       refetch();
     }
-  }, [loading, singlePokemonLoading, refetch]);
+  }, [
+    loading,
+    singlePokemonLoading,
+    opponentLoading,
+    userMovesLoading,
+    opponentMovesLoading,
+    refetch,
+  ]);
 
   function handleMove() {
     console.log("hi");
   }
+
   return (
     <>
       <div className="flex flex-wrap flex-col justify-end gap-3 h-screen">
         <div className="flex flex-wrap flex-row justify-end gap-3">
           <div className="flex flex-wrap flex-col basis-1/3 border-2 border-black rounded-lg justify-center bg-slate-200 gap-5">
-            <h1>{userPokemon.pokemonName}</h1>
+            <h1>{brocksPokemon.pokemonName}</h1>
             <p>
               HP
               <progress
@@ -45,9 +98,9 @@ const Battle = () => {
               ></progress>
             </p>
           </div>
-          {userPokemon.image && userPokemon.image[1] && (
+          {brocksPokemon.image && brocksPokemon.image[1] && (
             <div className="flex flex-wrap flex-col justify-end">
-              <img src={userPokemon.image[0]} className="w-64 h-auto"></img>
+              <img src={brocksPokemon.image[0]} className="w-64 h-auto"></img>
             </div>
           )}
         </div>
